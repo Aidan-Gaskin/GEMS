767896797c463fe0bea4213bafce453526355cc1
Aidan-Gaskin <aidan-gaskin@hotmail.com>
2023-02-27 12:02:39 +0000

    added switch case for inRefresh in refreshHandler

--- GEMS_GUI.java
+++ GEMS_GUI.java
@@ -828,7 +828,26 @@ public class GEMS_GUI
 			try
 			{
 				
+				switch(intRefresh)
+				{
+				case 1: System.out.println("\nCASE 1");
+				break;
+
+				case 2: System.out.println("\nCASE 2");
+				break;
 				
+				case 3: System.out.println("\nCASE 3");
+				break;
+
+				case 4: System.out.println("\nCASE 4");
+				break;
+				
+				case 5: System.out.println("\nCASE 5");
+				break;
+
+				case 6: System.out.println("\nCASE 6");
+				break;
+				}
 				
 			}
 			catch(Exception e)
